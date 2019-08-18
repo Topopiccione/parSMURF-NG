43ef9578547291fef25684a89775a297853b7e32
A. Petrini <alessandro.petrini@unimi.it>
2019-08-18 15:13:16 +0200

    added timing

--- src/parSMURFng.cpp
+++ src/parSMURFng.cpp
@@ -28,6 +28,9 @@ int main(int argc, char ** argv){
 	int	rank = 0;
 	int worldSize = 1;
 
+	double cacheTime, organTime, runnerTime;
+	Timer ttt;
+
 	MPI_Init( &argc, &argv );
 	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -41,15 +44,32 @@ int main(int argc, char ** argv){
 	std::vector<GridParams> gridParams;
 	ArgHandle commandLine( argc, argv, gridParams );
 	CommonParams commonParams = commandLine.processCommandLine( rank );
-	commandLine.printConfig(0,0);
+	if (rank == 0)
+		commandLine.printConfig(0,0);
 
 	// Megacache and organizer init
+	if (rank == 0)
+		ttt.startTime();
 	MegaCache mc(rank, worldSize, commonParams);
+	if (rank == 0) {
+		ttt.endTime();
+		cacheTime = ttt.duration();
+		ttt.startTime();
+	}
 	Organizer organ(rank, &mc, commonParams);
+	if (rank == 0) {
+		ttt.endTime();
+		organTime = ttt.duration();
+		ttt.startTime();
+	}
 
 	// Create an instance of the runner and launch the run
 	Runner runner(rank, worldSize, &mc, organ, commonParams, gridParams);
 	runner.go();
+	if (rank == 0) {
+		ttt.endTime();
+		runnerTime = ttt.duration();
+	}
 	runner.savePredictions();
 
 	// // Supertest to check if hyperSMURFcore works, using MyData1.txt and MyData1L.txt
@@ -71,6 +91,25 @@ int main(int argc, char ** argv){
 	// 	std::for_each(hsCore.class1Prob.begin(), hsCore.class1Prob.end(),[&](double val){std::cout << val << " ";} );
 	// 	std::cout << std::endl;
 	// }
+
+	if ((rank == 0) && (commonParams.timeFilename != "")) {
+		// Append computation time to log file, if specified by option --tttt
+		std::ofstream timeFile( commonParams.timeFilename.c_str(), std::ios::out | std::ios::app );
+		timeFile << "#Working procs: " << 1
+				<< " - #ensThreads: " << commonParams.nThr
+				<< " - #rfThreads: " << commonParams.rfThr
+				<< " - #folds: " << commonParams.nFolds
+				<< " - #parts: " << gridParams[0].nParts
+				<< " - #fp: " << gridParams[0].fp
+				<< " - #ratio: " << gridParams[0].ratio
+				<< " - #nTrees: " << gridParams[0].nTrees
+				<< " - #mtry: " << gridParams[0].mtry
+				<< " - Cache time: " << cacheTime
+				<< " - Organizer time: " << organTime
+				<< " - Computation time: " << runnerTime
+				<< std::endl;
+		timeFile.close();
+	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Finalize();
