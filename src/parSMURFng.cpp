8259e20545b4ec1a2d8592715573c8ca41bc6b55
A. P. (Unimi) <alessandro.petrini@unimi.it>
2019-08-19 18:34:52 +0200

    Minor cleanup of parSMURFng.cpp

--- src/parSMURFng.cpp
+++ src/parSMURFng.cpp
@@ -72,28 +72,8 @@ int main(int argc, char ** argv){
 	}
 	runner.savePredictions();
 
-	// // Supertest to check if hyperSMURFcore works, using MyData1.txt and MyData1L.txt
-	// {
-	// 	std::vector<size_t> posTrng;
-	// 	std::vector<size_t> posTest;
-	// 	std::vector<size_t> negTrng;
-	// 	std::vector<size_t> negTest;
-	// 	size_t cc = 0;
-	// 	for (;cc < 40; cc++) posTrng.push_back(cc);
-	// 	for (;cc < 50; cc++) posTest.push_back(cc);
-	// 	for (;cc < 850; cc++) negTrng.push_back(cc);
-	// 	for (;cc < 1000; cc++) negTest.push_back(cc);
-	//
-	// 	hyperSMURFcore hsCore(commonParams, gridParams[0], &mc, 0, 0);
-	// 	hsCore.train(posTrng, negTrng);
-	// 	hsCore.test(posTest, negTest);
-	//
-	// 	std::for_each(hsCore.class1Prob.begin(), hsCore.class1Prob.end(),[&](double val){std::cout << val << " ";} );
-	// 	std::cout << std::endl;
-	// }
-
 	if ((rank == 0) && (commonParams.timeFilename != "")) {
-		// Append computation time to log file, if specified by option --tttt
+		// Append computation time to log file, if specified by option 'exec':'timeFile'
 		std::ofstream timeFile( commonParams.timeFilename.c_str(), std::ios::out | std::ios::app );
 		timeFile << "#Working procs: " << 1
 				<< " - #ensThreads: " << commonParams.nThr
