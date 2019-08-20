94265b89cdc404e320967fc30ed9d3b6fa9e8ffc
A. P. (Unimi) <alessandro.petrini@unimi.it>
2019-08-20 09:31:30 +0200

    Optimizer: begin

---
+++ src/optimizer.h
@@ -0,0 +1,16 @@
+// parSMURFng
+// 2019 - Alessandro Petrini - AnacletoLAB - Universita' degli Studi di Milano
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "parSMURFUtils.h"
+#include "easylogging++.h"
+
+class Optimizer {
+public:
+	Optimizer();
+	~Optimizer();
+
+};
